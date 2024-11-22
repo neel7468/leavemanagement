b11009be5bc8c93e71ed4c6606bc8713521b598c
neelsharma7468@gmail.com <neelsharma7468@gmail.com>
2024-11-22 23:48:10 +0530

    first commit

---
+++ heads/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="footer-wrap pd-20 mb-20 card-box">
+				NLM Leave System <a href="https://aci.edu.gh/" target="_blank"><span>developed by </span> NEEL</a>
+			</div>
---
+++ staff/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="footer-wrap pd-20 mb-20 card-box">
+				NLM Leave System <a href="" target="_blank"><span>developed by </span> Neel</a>
+			</div>
